e4bae53bf8c52dee2bc21b25eb06937adf107e7f
greeshmamadhu25-lang <greeshmamadhu25@gmail.com>
2026-07-07 19:02:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+const messages = [
+  "I'm so lucky to have you mr and you too. ❤️",
+  "I hope you smiled today.",
+  "Don't forget to eat something delicious. 🍜",
+  "I'm always cheering for you, even from far away.",
+  "I can't wait to make more memories with you.",
+  "Thank you for being you. 💜",
+  "I'm really proud of you.",
+  "You mean more to me than words can say."
+];
+
+const button = document.querySelector(".enter-btn");
+const message = document.getElementById("message");
+
+button.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * messages.length);
+  message.textContent = messages[randomIndex];
+});
